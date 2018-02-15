a5c860653d3ad00da4fd7d338e14a5254f47e1e7
Madeline Wong <madelinegwong@outlook.com>
2018-02-15 18:57:21 -0500

    got data on the page from swapi.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
+const BASE_URL = 'https://swapi.co/api/';
+const PEOPLE_URL = `${BASE_URL}people/`;
+
 class App extends Component {
+  constructor(){
+    super();
+    this.state ={
+      people: []
+    }
+  }
+  componentDidMount(){
+    console.log('component did mount');
+    axios.get(PEOPLE_URL)
+    .then((response) => this.setState({people: response.data.results}))
+    .catch((err) => console.log(err));
+  }
   render() {
     return (
-      <div>
-        hello
-      </div>
+      <pre>
+       {JSON.stringify(this.state.people, null, 4)}
+      </pre>
     );
   }
 }
