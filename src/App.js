5e4fc9afe07f619eb2d4960a240d9d40a6950831
Madeline Wong <madelinegwong@outlook.com>
2018-02-15 19:39:17 -0500

    added people and loading state. Additionally, created a loading list.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,34 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
 
-const BASE_URL = 'https://swapi.co/api/';
+const BASE_URL = "https://swapi.co/api/";
 const PEOPLE_URL = `${BASE_URL}people/`;
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
-    this.state ={
-      people: []
-    }
+    this.state = {
+      people: [],
+      loading: true
+    };
   }
-  componentDidMount(){
-    console.log('component did mount');
-    axios.get(PEOPLE_URL)
-    .then((response) => this.setState({people: response.data.results}))
-    .catch((err) => console.log(err));
+  componentDidMount() {
+    console.log("component did mount");
+    axios
+      .get(PEOPLE_URL)
+      .then(response => this.setState({ loading: false, people: response.data.results }))
+      .catch(err => console.log(err));
   }
   render() {
+    if(this.state.loading){
+      return <h1>Loading...</h1>
+    }
+    const people = this.state.people.map(person => <li>{person.name}</li>);
     return (
-      <pre>
-       {JSON.stringify(this.state.people, null, 4)}
-      </pre>
-    );
+     <ul>
+       {people}
+    </ul>);
   }
 }
 
